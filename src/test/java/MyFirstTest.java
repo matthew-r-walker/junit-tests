3e85c699bce7ba05b93938768c008bdd1a390dc5
Matthew Walker <matthew.r.walker4@gmail.com>
2021-06-15 12:26:45 -0500

    completed junit warm up

---
+++ src/test/java/MyFirstTest.java
@@ -0,0 +1,45 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class MyFirstTest {
+
+    @Test
+    public void test1() {
+
+        assertEquals("Codeup", "CodeUp");
+
+    }
+
+    @Test
+    public void test2() {
+        List<String> languages = new ArrayList<>();
+        List<String> moreLanguages = new ArrayList<>();
+
+        assertNotSame(languages, moreLanguages);
+    }
+
+    @Test
+    public void test3(){
+        int[] numbers = {1, 2, 3};
+        int[] otherNumbers = new int[3];
+        otherNumbers[0] = 1;
+        otherNumbers[1] = 2;
+        otherNumbers[2] = 3;
+
+        assertArrayEquals(numbers, otherNumbers);
+    }
+
+    @Test
+    public void test4() {
+        String language = "PHP";
+
+
+        assertTrue(language.contains("H"));
+        assertFalse(language.contains("J"));
+    }
+
+}
