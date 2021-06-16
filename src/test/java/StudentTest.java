c95233fa20fa093082eb6bb466c9fc5383ed179b
Matthew Walker <matthew.r.walker4@gmail.com>
2021-06-16 11:07:26 -0500

    refactored student tests

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -5,45 +5,48 @@ import static org.junit.Assert.*;
 
 public class StudentTest {
 
-        Student student;
+
+    private Student bob;
+    private Student jim;
+    private Student bill;
 
     @Before
     public void setUp() {
-        this.student = new Student("Bob", 1);
+        this.bob = new Student("Bob", 1);
+        this.bill = new Student("Bill", 4);
+        this.jim = new Student("Jim", 2);
     }
 
 
     @Test
     public void studentConstructorTest() {
-        assertEquals(1, student.getId());
-        assertEquals("Bob", student.getName());
-        assertTrue(student.getGrades().isEmpty());
+        assertEquals(1, bob.getId());
+        assertEquals("Bob", bob.getName());
+        assertTrue(bob.getGrades().isEmpty());
     }
 
     @Test
     public void studentIdTest() {
-        assertEquals(1, student.getId());
-        Student jim  = new Student("Jim", 2);
+        assertEquals(1, bob.getId());
         assertEquals(2, jim.getId());
     }
 
     @Test
     public void studentNameTest() {
-        assertEquals("Bob", student.getName());
-        Student bill = new Student("Bill", 4);
+        assertEquals("Bob", bob.getName());
         assertEquals("Bill", bill.getName());
     }
 
     @Test
     public void studentAddGradeTest() {
-        student.addGrade(80);
-        student.addGrade(73);
-        student.addGrade(85);
+        bob.addGrade(80);
+        bob.addGrade(73);
+        bob.addGrade(85);
+        assertEquals(3, bob.getGrades().size());
 
-        assertEquals(3, student.getGrades().size());
-        student.addGrade(87);
-        assertEquals(4, student.getGrades().size());
-        Student bill = new Student("Bill", 4);
+        bob.addGrade(87);
+        assertEquals(4, bob.getGrades().size());
+
         bill.addGrade(74);
         assertEquals(1, bill.getGrades().size());
 
@@ -51,19 +54,19 @@ public class StudentTest {
 
     @Test
     public void studentGetGradesTest() {
-        assertEquals(0, student.getGrades().size());
-        student.addGrade(60);
-        assertEquals(1, student.getGrades().size());
+        assertEquals(0, bob.getGrades().size());
+        bob.addGrade(60);
+        assertEquals(1, bob.getGrades().size());
     }
 
     @Test
     public void studentGetGradeAverageTest() {
-        student.addGrade(80);
-        student.addGrade(85);
-        student.addGrade(90);
-        assertEquals(85, student.getGradeAverage(), 0);
-        student.addGrade(10);
-        assertEquals(66.25, student.getGradeAverage(), 0);
+        bob.addGrade(80);
+        bob.addGrade(85);
+        bob.addGrade(90);
+        assertEquals(85, bob.getGradeAverage(), 0);
+        bob.addGrade(10);
+        assertEquals(66.25, bob.getGradeAverage(), 0);
     }
 
 
